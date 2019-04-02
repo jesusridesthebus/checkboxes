17c24337a7cb748940cac17f45cad97ccba9a5e1
Liz Kelley and Marc Davies <marc@>
2019-04-02 13:18:11 -0700

    initialize html file with checkbox survey and script with function that collects user inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(function() {
-
+  $("form#transportation_survey").submit(function(event){
+    event.preventDefault();
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      var workTransportationMode = $(this).val();
+      $('#work-responses').append(workTransportationMode + "<br>");
+    });
+    $('#transportation_survey').hide();
+  });
 });
