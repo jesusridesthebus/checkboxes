1811677000558fe75ed8471eada932f51ab9c43a
Liz Kelley and Marc Davies <liz@>
2019-04-02 15:36:40 -0700

    remove console logs

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,11 @@ $(function() {
     var luckyCount = [];
     var unluckyCount = [];
 
-    console.log("luckyCount", luckyCount);
-    console.log("unluckyCount", unluckyCount);
-
     $("input:checkbox[name=lucky]:checked").each(function() {
       var luckyResponse = luckyCount.push($(this).val());
-      console.log("luckyResponse", luckyResponse);
     });
     $("input:checkbox[name=unlucky]:checked").each(function() {
       var unluckyResponse = unluckyCount.push($(this).val());
-      console.log("unluckyResponse", unluckyResponse);
     });
 
     if(luckyCount.length > unluckyCount.length) {
